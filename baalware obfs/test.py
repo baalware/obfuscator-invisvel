5205d54e71b24262a0c429369fa3a1885dddfb20
baalware <84687049+baalware@users.noreply.github.com>
2023-05-08 02:12:45 -0300

    Add files via upload

---
+++ baalware obfs/test.py
@@ -0,0 +1,19 @@
+import colorama
+import random
+import time
+import os
+colorama.init()
+
+cores = [
+    colorama.Fore.RED,
+    colorama.Fore.GREEN,
+    colorama.Fore.YELLOW,
+    colorama.Fore.BLUE,
+    colorama.Fore.MAGENTA,
+    colorama.Fore.CYAN,
+]
+while True:
+    cor = random.choice(cores)
+    print(cor + " 1 0 1 BAALWARE TEST - BAALWARE TEST - BAALWARE TEST - BAALWARE TEST - BAALWARE TEST - BAALWARE TEST - BAALWARE TEST - BAALWARE TEST 0 1 0 1")
+    # Aguarda um curto intervalo de tempo
+    time.sleep(0.0)
